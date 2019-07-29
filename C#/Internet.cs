0c8b093b69d078997b79655580df4cafe68975d0
guzlewski <45144232+guzlewski@users.noreply.github.com>
2019-07-29 19:23:24 +0200

    [[p[

---
+++ C#/Internet.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+
+namespace EasyLicense
+{
+    [SecuritySafeCritical]
+    class Internet
+    {
+        [DllImport("wininet.dll")]
+        protected static extern bool InternetGetConnectedState(out int Description, int ReservedValue);
+        internal static bool Connection()
+        {
+            int Desc;
+            return InternetGetConnectedState(out Desc, 0);
+        }
+    }
+}
